2124b27820744963a01c9809185931a9148b88b4
jefejeff <54154830+jefejeff@users.noreply.github.com>
2020-01-23 10:32:00 -0600

    Working on sepia

--- PictureLab/classes/Collage.java
+++ PictureLab/classes/Collage.java
@@ -8,6 +8,7 @@ public class Collage
 {
     public static void main(String[] args)
     {
+        //initializing all the pictures
         Picture me1 = new Picture("me.jpg");
         Picture me2 = new Picture("me.jpg");
         Picture me3 = new Picture("me.jpg");
@@ -17,18 +18,29 @@ public class Collage
         Picture me7 = new Picture("me.jpg");
         Picture me8 = new Picture("me.jpg");
         Picture me9 = new Picture("me.jpg");
-        me2.mirrorVerticalRightToLeft();
-        me3.zeroBlue();
-        Picture canvas = new Picture(1200, 1500);
-        canvas.cropAndCopy(me1, 0, 399, 0, 499, 0, 0);
-        canvas.cropAndCopy(me2, 0, 399, 0, 499, 0, 500);
-        canvas.cropAndCopy(me3, 0, 399, 0, 499, 400, 0);
-        canvas.cropAndCopy(me4, 0, 399, 0, 499, 400, 500);
-        canvas.cropAndCopy(me5, 0, 399, 0, 499, 800, 500);
-        canvas.cropAndCopy(me6, 0, 399, 0, 499, 400, 1000);
-        canvas.cropAndCopy(me7, 0, 399, 0, 499, 800, 1000);
-        canvas.cropAndCopy(me8, 0, 399, 0, 499, 0, 1000);
-        canvas.cropAndCopy(me9, 0, 399, 0, 499, 800, 0);
+
+        //running the various methods and filters on the pictures
+        me1.posterize();
+        me2.negate();
+        me3.keepOnlyBlue();
+        me4.mirrorVertical();
+        //leave me5 in original form
+        me6.mirrorVerticalRightToLeft();
+
+        Picture canvas = new Picture(1240, 1540);
+        canvas.setBlack();
+        //using CropAndCopy to fit all the pictures onto the collage canvas
+        canvas.cropAndCopy(me1, 0, 399, 0, 499, 10, 10);
+        canvas.cropAndCopy(me2, 0, 399, 0, 499, 10, 520);
+        canvas.cropAndCopy(me3, 0, 399, 0, 499, 10, 1030);
+        canvas.cropAndCopy(me4, 0, 399, 0, 499, 420, 10);
+        canvas.cropAndCopy(me5, 0, 399, 0, 499, 420, 520);
+        canvas.cropAndCopy(me6, 0, 399, 0, 499, 420, 1030);
+        canvas.cropAndCopy(me7, 0, 399, 0, 499, 830, 10);
+        canvas.cropAndCopy(me8, 0, 399, 0, 499, 830, 520);
+        canvas.cropAndCopy(me9, 0, 399, 0, 499, 830, 1030);
+
+        //creating a "picture frame" around all the pictures"
         canvas.explore();
     }
 }
--- PictureLab/classes/Picture.java
+++ PictureLab/classes/Picture.java
@@ -391,6 +391,147 @@ public class Picture extends SimplePicture
         }
     }
 
+    /** Method to set a black screen */
+    public void setBlack()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                pixelObj.setRed(0);
+                pixelObj.setGreen(0);
+                pixelObj.setBlue(0);
+            }
+        }
+    }
+
+    /** Method to posterize an image */
+    public void posterize()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        //sets posterize values
+        int red1 = 32;
+        int red2 = 96;
+        int red3 = 160;
+        int red4 = 224;
+        int green1 = 32;
+        int green2 = 96;
+        int green3 = 160;
+        int green4 = 224;
+        int blue1 = 32;
+        int blue2 = 96;
+        int blue3 = 160;
+        int blue4 = 224;
+
+        //runs through and posterizes for red values
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                int red = pixelObj.getRed();
+                if(0 <= red && red <= 63)
+                {
+                    pixelObj.setRed(red1);
+                }
+                else if(64 <= red && red <= 127)
+                {
+                    pixelObj.setRed(red2);
+                }
+                else if(128 <= red && red <= 191)
+                {
+                    pixelObj.setRed(red3);
+                }
+                else if(192 <= red && red <= 255)
+                {
+                    pixelObj.setRed(red4);
+                }
+            }
+        }
+
+        //runs through and posterizes for blue values
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                int blue = pixelObj.getBlue();
+                if(0 <= blue && blue <= 63)
+                {
+                    pixelObj.setBlue(blue1);
+                }
+                else if(64 <= blue && blue <= 127)
+                {
+                    pixelObj.setBlue(red2);
+                }
+                else if(128 <= blue && blue <= 191)
+                {
+                    pixelObj.setBlue(blue3);
+                }
+                else if(192 <= blue && blue <= 255)
+                {
+                    pixelObj.setBlue(blue4);
+                }
+            }
+        }
+
+        //runs through and posterizes for green values
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                int green = pixelObj.getGreen();
+                if(0 <= green && green <= 63)
+                {
+                    pixelObj.setGreen(green1);
+                }
+                else if(64 <= green && green <= 127)
+                {
+                    pixelObj.setGreen(green2);
+                }
+                else if(128 <= green && green <= 191)
+                {
+                    pixelObj.setGreen(green3);
+                }
+                else if(192 <= green && green <= 255)
+                {
+                    pixelObj.setGreen(green4);
+                }
+            }
+        }
+    }
+
+    /** Method to turn the picture to sepia */
+    public void sepia()
+    {
+        Pixel[][] pixels = this.getPixels2D();
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                int red = pixelObj.getRed();
+                int blue = pixelObj.getBlue();
+                int green = pixelObj.getGreen();
+                int avg = (red + blue + green) / 3;
+                pixelObj.setRed(avg);
+                pixelObj.setGreen(avg);
+                pixelObj.setBlue(avg);
+            }
+        }
+        for (Pixel[] rowArray : pixels)
+        {
+            for (Pixel pixelObj : rowArray)
+            {
+                int red = pixelObj.getRed();
+                int blue = pixelObj.getBlue();
+                int green = pixelObj.getGreen();
+                int avg = (red + blue + green) / 3;
+                pixelObj.setRed(avg);
+                pixelObj.setGreen(avg);
+                pixelObj.setBlue(avg);
+            }
+        }
+    }
+
     /* Main method for testing - each class in Java can have a main 
      * method 
      */
